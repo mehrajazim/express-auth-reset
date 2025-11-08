9bebda6191b178fdfbd084d9c7b9e87de0678db1
AzimMehraj <mehrajazim@gmail.com>
2025-11-08 21:25:28 +0600

    added verification system

--- express-auth-reset/index.js
+++ express-auth-reset/index.js
@@ -1,4 +1,5 @@
 const db = require('./db');
+const bcrypt = require('bcrypt');
 
 const express = require('express');
 
@@ -23,6 +24,32 @@ app.get('/db-test', async(req,res) =>{
     res.status(500).json({error: err.message});
   }
 })
+
+app.post('/register', async( req, res) =>{
+  try{
+  const { username, email, password } = req.body;
+  }
+  catch(err){
+    return res.status(400).json({error: 'Invalid JSON'});
+  }
+  if(!username || !email || !password){
+    return res.status(400).json({error: 'Missing required fields'});
+  }
+  
+  try {
+      const [existing] = await db.query('Select id from users where email = ? or username = ?', [email,username]);
+      if (existing.length > 0){
+        return res.status(400).json({ error: 'User already exists'});
+      }
+      const hashed = await bcrypt.hash(password, 10);
+      await db.query('Insert into users (username, email, password) values (?,?,?)', [username,email,hashed]);
+      res.status(201).json({ message:'User registered successfully'});
+  }
+  catch (err){
+    res.status(500).json({error: err.message});
+  }
+})
+
 
 app.listen(3000,() => {
     console.log("server running on localhost:3000");
