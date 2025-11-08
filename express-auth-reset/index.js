e803e68c7227205e401c6f4b861bea203ff23b72
AzimMehraj <mehrajazim@gmail.com>
2025-11-08 16:40:55 +0600

    connect to db

--- express-auth-reset/index.js
+++ express-auth-reset/index.js
@@ -1,3 +1,5 @@
+const db = require('./db');
+
 const express = require('express');
 
 const app = express();
@@ -11,6 +13,16 @@ app.get('/', (req,res) =>{
 app.post('/echo', (req, res) => {
   res.json({ youSent: req.body });
 });
+
+app.get('/db-test', async(req,res) =>{
+  try {
+    const [rows] = await db.query('Select 1+1 AS solution');
+    res.json({ mysqlWorks: rows[0].solution });
+  }
+  catch (err){
+    res.status(500).json({error: err.message});
+  }
+})
 
 app.listen(3000,() => {
     console.log("server running on localhost:3000");
